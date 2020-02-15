b73fcf2adb9c1b32672d7a6929b7b3b89fa57282
Rasmus <rasmusbamse@hotmail.com>
2020-02-15 15:40:10 +0100

    Initial Commit

---
+++ conversiontools.py
@@ -0,0 +1,57 @@
+from enum import Enum
+from pathlib import Path
+import wget
+import zipfile
+
+class ConvStatus(Enum):
+	CONV_VERSION_NOT_FOUND = 0
+	CONV_VERSION_FOUND = 1
+
+class ConversionTools:
+	"""This class makes sure that the wanted version conversion tools are downloaded and initialized"""
+	def __init__(self, game, version):
+		self.game = game
+		self.version = self._gameVerToConvVer(version)
+		self.versionStr = self._convVerToString(".")
+		self.status = self._checkConv()
+		if not self.status == ConvStatus.CONV_VERSION_FOUND:
+			self._dlConv()
+
+	def _gameVerToConvVer(self, version):
+		if version == 30:
+			return [2,5]
+		if version == 31:
+			return [2,6]
+		if version <= 34: # ETS2 1.32-1.34
+			return [2,7]
+		if version == 35:
+			return [2,8]
+		if version == 36:
+			return [2,10]
+		raise UserWarning("gameVerToConvVer(): Unsupported ETS2 version: "+version)
+
+	def _convVerToString(self, separator = "."):
+		out = ""
+		for versionDigit in self.version:
+			# URL uses underscore as version digit separator
+			if not out == "": out = out + separator
+			out = out + str(versionDigit)
+		return out
+
+	def _checkConv(self):
+		path = Path("scsconv/"+self.versionStr)
+		if path.is_file():
+			return ConvStatus.CONV_VERSION_FOUND
+		return ConvStatus.CONV_VERSION_NOT_FOUND
+
+	def _dlConv(self):
+		versionUrl = self._convVerToString("_")
+		convFile = Path("scsconv/conversion_tools_%s.zip" % versionUrl)
+		if not convFile.is_file():
+			wget.download("http://download.eurotrucksimulator2.com/conversion_tools_%s.zip" % versionUrl, "scsconv")
+
+		with zipfile.ZipFile(convFile,"r") as file:
+			file.extractall(Path("scsconv/%s/" % self.versionStr))
+
+	def getConvPath(self):
+		return str(Path("scsconv/%s/" % self.versionStr))
---
+++ main.py
@@ -0,0 +1,66 @@
+from enum import Enum
+from conversiontools import ConversionTools
+from pathlib import Path
+import os
+import subprocess
+import sys
+import hashlib
+
+class Compression(Enum):
+	"""Enums for what compression the mod should be packed with.
+	FOLDER means that the mod is not packed as an archive file, but be stored unpacked inside of a folder."""
+	FOLDER = 0
+	NONE = 1
+	DEFLATE = 2
+	BZIP = 3
+
+class Game(Enum):
+	ETS2 = 0
+	ATS = 1
+
+class Converter:
+	def __init__(self, argConvtool, argOptions = None):
+		self.options = {
+			"modname": "My mod!",
+			"compression": Compression.FOLDER,
+			"game": Game.ETS2,
+			"gameversion": 36,
+			"directory": "tests/example_mod/",
+			"staticdirectory": None
+		}
+		self.convtool = argConvtool
+
+	def getUid(self):
+		return "ct_" + hashlib.md5(self.options['modname'].encode("UTF-8")).hexdigest()
+
+	@staticmethod
+	def createSymlink(src, dest):
+		if os.path.isdir(dest):
+			os.remove(dest)  # use os.remove instead os.unlink, as we can't remove mklink junction with os.unlink.
+
+		if sys.platform == "win32":
+			subprocess.check_call(["mklink", "/J", str(dest), str(src)], shell=True)
+		else:
+			os.symlink(src, dest)
+
+	def ensureMount(self):
+		"""Ensure that the symlink name in /extra_mount.txt exists"""
+		with open(Path(self.convtool.getConvPath() + "/extra_mount.txt"), "a") as file:
+			for line in file:
+				if self.getUid() in line:
+					break
+			else:
+				file.write("%s" % self.getUid())
+
+
+def main():
+	convtool = ConversionTools(Game.ETS2, 36)
+	convert = Converter(convtool)
+	Converter.createSymlink(
+		Path(convert.options['directory']),
+		Path(convtool.getConvPath() + "/%s/" % convert.getUid()))
+	convert.ensureMount()
+
+
+if __name__ == "__main__":
+	main()
